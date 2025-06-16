d31c3cbb239a0fbbecd91ad7d71f09d0835f00da
Artem Uholkov <artem.ugolkov@ukr.net>
2025-06-16 10:30:13 +0300

    asd

---
+++ js/reviews.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const reviewsSwiper = new Swiper('.reviews-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    preventClicks: false,
+    preventClicksPropagation: false,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+});
